d954f32fc8a804c30439c8037e0f1a20b75c75cf
rodrick <jckmyemail@163.com>
2020-09-29 19:27:26 +0800

    修改侧边栏和add

--- docs/.vuepress/sidebar.js
+++ docs/.vuepress/sidebar.js
@@ -1,28 +1,33 @@
 module.exports = {
   '/frontEnd/vue/': [{
-    title: 'vue全家桶',  
-    children: [  
+    title: 'vue全家桶',
+    children: [
       'vue-cli',
       'vue-router',
       'vuex'
     ]
   }],
-  '/frontEnd/js/': [{
-    title: 'ES6',  
-    children: [  
-      'ES6核心语法',
-      '模块化&Webpack',
-      'Promise&async&await',
-      'WebSocket原理浅析',
-      'axios',
-      '变量提升'
+  '/frontEnd/': [{
+    title: 'ES6',
+    children: [
+      'js/ES6核心语法',
+      'js/模块化&Webpack',
+      'js/Promise&async&await',
+      'js/WebSocket原理浅析',
+      'js/axios',
+      'js/变量提升'
+    ]
+  },{
+    title: 'CSS',
+    children: [
+      'css/各种页面常见布局'
     ]
   }],
   '/dataBase/': [{
-    title: 'Mysql',  
-    children: [  
+    title: 'Mysql',
+    children: [
       'mysql-根据指定字符分隔字符串然后循环做操作',
       '查询函数LOCATE、POSITION、INSTR、FIND_IN_SET、IN、LIKE'
     ]
-  }]
+  }],
 }
