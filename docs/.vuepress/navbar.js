8e7d3d1c29fea52e173987ba20f542fcec6c2f89
rodrick <jckmyemail@163.com>
2020-10-03 19:16:38 +0800

    axihe

--- docs/.vuepress/navbar.js
+++ docs/.vuepress/navbar.js
@@ -7,19 +7,39 @@ module.exports = [
     text: 'D&T',
     icon: 'reco-document',
     items: [
-      { text: '现代JavaScript教程', link: 'https://zh.javascript.info/' },
-      { text: 'Vue', link: 'https://cn.vuejs.org/' },
-      { text: 'Vue3', link: 'https://composition-api.vuejs.org//' },
-      { text: 'Webpack', link: 'https://www.webpackjs.com/' },
-      { text: 'MDN', link: 'https://developer.mozilla.org/zh-CN/' },
-      { text: 'Node中文网', link: 'http://nodejs.cn/api/' },
-      { text: 'LeetCode', link: 'https://leetcode-cn.com/' },
-      { text: '牛客网', link: 'https://www.nowcoder.com/' },
-      { text: 'React', link: 'https://react.docschina.org/' },
-      { text: '小程序', link: 'https://developers.weixin.qq.com/miniprogram/dev/framework/' },
-      { text: 'ES6', link: 'https://es6.ruanyifeng.com/' },
-      { text: 'bejson', link: 'https://www.bejson.com/' },
-      { text: 'FineReport', link: 'https://help.finereport.com/' },
+      {
+        text: '官方文档',
+        items: [
+          { text: 'Vue', link: 'https://cn.vuejs.org/' },
+          { text: 'Vue3', link: 'https://composition-api.vuejs.org//' },
+          { text: 'Webpack', link: 'https://www.webpackjs.com/' },
+          { text: 'MDN', link: 'https://developer.mozilla.org/zh-CN/' },
+          { text: 'Node中文网', link: 'http://nodejs.cn/api/' },
+          { text: 'React', link: 'https://react.docschina.org/' },
+          { text: '小程序', link: 'https://developers.weixin.qq.com/miniprogram/dev/framework/' },
+          { text: 'FineReport', link: 'https://help.finereport.com/' },
+        ]
+      },
+      {
+        text: '学习面试',
+        items: [
+          { text: '现代JavaScript教程', link: 'https://zh.javascript.info/' },
+          { text: 'ES6', link: 'https://es6.ruanyifeng.com/' },
+          { text: '阿西河', link: 'https://www.axihe.com/' },
+          { text: 'LeetCode', link: 'https://leetcode-cn.com/' },
+          { text: '牛客网', link: 'https://www.nowcoder.com/' },
+          
+        ]
+      },
+      {
+        text: '工具',
+        items: [
+          { text: 'bejson', link: 'https://www.bejson.com/' },
+        ]
+      }
+
+
+
     ]
   },
   {
